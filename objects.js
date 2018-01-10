bf119d6104b500182fe0d8278aee5b10f9217e3f
Charles Mainegra <cmainegra@gmail.com>
2018-01-10 00:18:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,11 @@ function updatePlaylist(playlist, artistName, songTitle) {
  return Object.assign({}, playlist, {[artistName]: songTitle});
 }
 function removeFromPlaylist(playlist, artistName) {
+<<<<<<< HEAD
  delete playlist[artistName];
  return playlist;
+=======
+ var newPlaylist = delete playlist.artistName;
+ return newPlaylist;
+>>>>>>> 08d97abb1b57b6ca59f14667b57fe77a4f047312
 }
